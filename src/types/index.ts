ca928e4657b9387bdb34d09e990b0c051c234b83
KimDa <baronmakaron3000@yandex.ru>
2024-12-09 09:00:39 +0300

    предварительная типизация

--- src/types/index.ts
+++ src/types/index.ts
@@ -15,3 +15,19 @@ export interface IOrderInfo {
 	total: number;
 	items: string[];
 }
+
+export interface ICart {
+    items: IProduct[];
+    total: number;
+}
+
+export interface IPage {
+    items: IProduct[];
+    counter: number;
+}
+
+export type TCart = Pick<IProduct, 'id' | 'title' | 'price'>;
+export type TCard = Pick<IProduct, 'description'| 'image' | 'category' | 'title' |'price'>;
+export type TFormDelivery = Pick<IOrderInfo, 'payment' | 'address' >;
+export type TFormContact = Pick<IOrderInfo, 'email' | 'phone'>;
+
